5edea3c042e1ae3d50eb739d53a610f1e25b9641
Abdelilah Lbardi <lbardi.abdelilah@gmail.com>
2023-12-01 02:43:56 +0100

    action that retrieves a game or create new one

---
+++ app/Actions/Games/FindUnfinishedGameOrInitiateOne.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Actions\Games;
+
+use App\Actions\Prizes\GetRandomPrize;
+use App\Models\Game;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+
+class FindUnfinishedGameOrInitiateOne
+{
+    public function __construct(public GetRandomPrize $getRandomPrize)
+    {}
+
+    public function execute(string $account, int $campaignId, string $segment = 'low'): Game|Model|null
+    {
+        $game = Game::query()
+            ->when($segment, fn (Builder $q, $segment) => $q->whereRelation('prize', 'level', $segment))
+            ->where([
+                'account' => $account,
+                'campaign_id' => $campaignId
+            ])
+            ->with('prize')
+            ->first();
+
+        if (!empty($game)) {
+            return $game;
+        }
+
+        $prize = $this->getRandomPrize->execute($segment, ['id', 'daily_volume']);
+
+        $game = Game::query()
+            ->create([
+                'account' => $account,
+                'campaign_id' => $campaignId,
+                'prize_id' => $prize->id
+            ]);
+
+        /*
+         * Performance TIP:
+         * No need to load again the prize model
+         * Just by assigning, it will be accessible
+         * exactly like ->load('prize')
+         */
+        $game->prize = $prize;
+
+        return $game;
+    }
+}
