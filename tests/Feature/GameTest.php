7fc48381a7affe15aecb8b5c598280920b59ecbb
Abdelilah Lbardi <lbardi.abdelilah@gmail.com>
2023-12-01 15:52:48 +0100

    test game play when prize daily volume exceeded

--- tests/Feature/GameTest.php
+++ tests/Feature/GameTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Actions\Games\CheckGameCompletion;
 use App\Models\Campaign;
 use Database\Factories\CampaignFactory;
+use Database\Factories\PrizeFactory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Carbon;
 use Illuminate\Testing\TestResponse;
@@ -44,6 +45,25 @@ class GameTest extends TestCase
             ->assertViewIs('frontend.index')
             ->assertViewHas('config', json_encode([
                 'message' => trans('campaign.expired')
+            ]));
+    }
+
+    public function test_game_cannot_be_played_when_prize_daily_volume_has_exceeded()
+    {
+        $prize = PrizeFactory::new()
+            ->campaign($this->campaign)
+            ->create([
+                'daily_volume' => 2
+            ]);
+
+        cache([$prize->getCacheKey() => 2]);
+
+        $this->mockRandomPrizeAction();
+
+        $this->loadCampaignEndpoint($this->campaign->slug, 'test-account')
+            ->assertViewIs('frontend.index')
+            ->assertViewHas('config', json_encode([
+                'message' => trans('prize.daily_volume_exceeded', ['hours' => 22, 'minutes' => 30])
             ]));
     }
 
