69fec56669f281384619e4e8507a2c05ed645cf3
Abdelilah Lbardi <lbardi.abdelilah@gmail.com>
2023-12-01 04:51:03 +0100

    Moving export button to the livewire component to benefit from models binding

--- resources/views/backstage/partials/tables/top.blade.php
+++ resources/views/backstage/partials/tables/top.blade.php
@@ -1,3 +1,16 @@
+<div class="col-span-6 text-right pb-8">
+    <form method="POST" wire:submit.prevent="export">
+        <div class="grid grid-cols-4 gap-4 items-start pt-5">
+            <div class="col-start-2 col-span-3">
+                <button type="submit" class="submit-button">
+                    Export csv
+                </button>
+            </div>
+        </div>
+    </form>
+</div>
+
+
 <div class="flex justify-between pb-4 h-14">
     <h1 class="m-0 p-0">{{ ucfirst($resource) }}</h1>
     @if(isset($crmHeader))
