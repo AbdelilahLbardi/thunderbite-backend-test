3c60765e812ab05c63317f46915d678a405aced8
Abdelilah Lbardi <lbardi.abdelilah@gmail.com>
2023-12-01 02:50:44 +0100

    sets up api controller logic

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -2,18 +2,53 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Actions\Prizes\CheckDailyVolume;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Cache;
+use App\Models\Game;
 
 class ApiController extends Controller
 {
-    public function flip()
+    public function flip(CheckDailyVolume $checkDailyVolume)
     {
-        $currentMove = (Cache::get(request('gameId')) ?? 0) + 1;
-        Cache::put(request('gameId'), $currentMove);
+        $data = request()->validate([
+            'gameId' => 'required',
+            'tileIndex' => 'required|number|min:0|max:24'
+        ]);
+
+        /** @var Game $game */
+        $game = Game::query()
+            ->with('prize:id,tile_image,daily_volume')
+            ->select('id', 'prize_id', 'revealed_tiles')
+            ->findOrFail($data['gameId']);
+
+        $prize = $game->prize;
+
+        if (! empty($prize->daily_volume)) {
+
+            $dailyVolume = $checkDailyVolume->execute($prize->getCacheKey(), $prize->daily_volume);
+
+            if ($dailyVolume->isInvalid()) {
+                return [
+                    'message' => $dailyVolume->message()
+                ];
+            }
+        }
+
+        $revealedTiles = $game->revealed_tiles ?? [];
+
+        $revealedTiles[] = [
+            'index' => $data['tileIndex'],
+            'image' => $prize->tile_image
+        ];
+
+        $game->update([
+            'revealed_tiles' => $revealedTiles
+        ]);
+
+        $currentMove = count($revealedTiles);
 
         return [
-            'tileImage' => asset('assets/'.mt_rand(1, 7).'.png'),
-        ] + ($currentMove >= 10 ? ['message' => 'you lost'] : []);
+            'tileImage' => $prize->tile_image,
+        ] + ($currentMove >= 3 ? ['message' => $prize->description ?? trans('prize.default_win_message')] : []);
     }
 }
